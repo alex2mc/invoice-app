0e26bb6ff6cd03a78b07f5bf69e42a69ad58791e
SashaPltnchnk <ZDubinaQ@gmail.com>
2019-08-07 15:51:12 +0300

    store: reducers: products) handled products actions in reducer

--- src/store/reducers/products.js
+++ src/store/reducers/products.js
@@ -1,4 +1,8 @@
-
+import {
+  FETCH_PRODUCTS,
+  FETCH_PRODUCTS_SUCCESS,
+  FETCH_PRODUCTS_FAILURE
+} from '../actions/products';
 
 const initialState = {
   products: [],
@@ -8,6 +12,26 @@ const initialState = {
 
 export default function rootReducer(state = initialState, action) {
   switch (action.type) {
+
+    case FETCH_PRODUCTS:
+      return {
+        ...state,
+        isLoading: true,
+        error: null
+      };
+    case FETCH_PRODUCTS_SUCCESS:
+      return {
+        products: [...action.payload],
+        isLoading: false,
+        error: null
+      };
+    case FETCH_PRODUCTS_FAILURE:
+      return {
+        products: [],
+        isLoading: false,
+        error: action.payload
+      };
+
     default:
       return state;
   }
