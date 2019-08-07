e4c81c43686641e3779db59536e9d06a23a895c6
SashaPltnchnk <ZDubinaQ@gmail.com>
2019-08-07 14:21:05 +0300

    feat (components: NavTabs: NavTabs) implemented simple appearance of NavTabs

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import NavTabs from './components/NavTabs/NavTabs'
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <NavTabs />
+    </div>
+  );
+}
+
+export default App;
