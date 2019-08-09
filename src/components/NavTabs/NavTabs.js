1b71d67f754467f0d2b1ef8e0587126980eb4c03
ZI <ZDubinaQ@gmail.com>
2019-08-09 11:30:50 +0300

    upd(components: NavTabs: NavTabs) remade Material Tabs into Toolbar

--- src/components/NavTabs/NavTabs.js
+++ src/components/NavTabs/NavTabs.js
@@ -1,102 +1,43 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import { makeStyles } from '@material-ui/core/styles';
+
 import AppBar from '@material-ui/core/AppBar';
-import Tabs from '@material-ui/core/Tabs';
-import Tab from '@material-ui/core/Tab';
+import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import Box from '@material-ui/core/Box';
 
-import MainPage from '../MainPage/MainPage';
-import Products from '../Products/Products';
-import Customers from '../Customers/Customers';
-import Invoices from '../Invoices/Invoices';
-import ViewMode from "../Invoices/Modes/ViewMode";
+import { Link } from 'react-router-dom';
 
-import { Link, } from 'react-router-dom';
 
-function TabPanel(props) {
-  const { children, value, index, ...other } = props;
 
-  return (
-    <Typography
-      component="div"
-      role="tabpanel"
-      hidden={value !== index}
-      id={`nav-tabpanel-${index}`}
-      aria-labelledby={`nav-tab-${index}`}
-      {...other}
-    >
-      <Box p={3}>{children}</Box>
-    </Typography>
-  );
-}
 
-TabPanel.propTypes = {
-  children: PropTypes.node,
-  index: PropTypes.any.isRequired,
-  value: PropTypes.any.isRequired,
-};
-
-function a11yProps(index) {
-  return {
-    id: `nav-tab-${index}`,
-    'aria-controls': `nav-tabpanel-${index}`,
-  };
-}
-
-function LinkTab(props) {
-  return (
-    <Tab
-      component="a"
-      onClick={event => {
-        event.preventDefault();
-      }}
-      {...props}
-    />
-  );
-}
-
-const useStyles = makeStyles(theme => ({
-  root: {
-    flexGrow: 1,
-    backgroundColor: theme.palette.background.paper,
-  },
-}));
 
 export default function NavTabs() {
-  const classes = useStyles();
-  const [value, setValue] = React.useState(0);
 
-  function handleChange(event, newValue) {
-    setValue(newValue);
+  const toolbarStyles = {
+    display: 'flex',
+    justifyContent: 'space-between'
   }
 
+
   return (
-    <div className={classes.root}>
+    <div >
       <AppBar position="static">
-        <Tabs
-          variant="fullWidth"
-          value={value}
-          onChange={handleChange}
-          aria-label="nav tabs"
-        >
+        <Toolbar variant="dense" style={toolbarStyles}>
           <Link to="/">
-            <Tab label="Logo" {...a11yProps(0)} />
+            <Typography variant="h6" color="textPrimary"> Logo</Typography>
           </Link>
           <Link to="/products">
-            <Tab label="Products"  {...a11yProps(1)} />
+            <Typography variant="h6" color="textPrimary"> Products</Typography>
           </Link>
           <Link to="/customers">
-            <Tab label="Customers" href="/customers" {...a11yProps(2)} />
+            <Typography variant="h6" color="textPrimary"> Customers</Typography>
           </Link>
           <Link to="/invoices">
-            <Tab label="Invoices" href="/invoices" {...a11yProps(3)} />
+            <Typography variant="h6" color="textPrimary"> Invoices</Typography>
           </Link>
           <Link to="/newinvoice">
-            <Tab label="+ New Invoice" href="/newinvoice" {...a11yProps(4)} />
+            <Typography variant="h6" color="textPrimary"> + New Invoice</Typography>
           </Link>
-        </Tabs>
+        </Toolbar>
       </AppBar>
 
     </div>
