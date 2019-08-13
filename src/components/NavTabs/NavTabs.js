e7e4b6572d20ae94c8506a0b247e0fffa5fa28b2
ZI <ZDubinaQ@gmail.com>
2019-08-13 13:20:17 +0300

    upd(components:NavTabs:NavTabs) refactor into class based component

--- src/components/NavTabs/NavTabs.js
+++ src/components/NavTabs/NavTabs.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
@@ -10,36 +10,40 @@ import { Link } from 'react-router-dom';
 
 
 
-export default function NavTabs() {
+class NavTabs extends Component {
+  render () {
 
-  const toolbarStyles = {
-    display: 'flex',
-    justifyContent: 'space-between'
+    const toolbarStyles = {
+      display: 'flex',
+      justifyContent: 'space-between'
+    }
+
+
+    return (
+      <div>
+        <AppBar position="static">
+          <Toolbar variant="dense" style={toolbarStyles}>
+            <Link to="/">
+              <Typography variant="h6" color="textPrimary"> Logo</Typography>
+            </Link>
+            <Link to="/products">
+              <Typography variant="h6" color="textPrimary"> Products</Typography>
+            </Link>
+            <Link to="/customers">
+              <Typography variant="h6" color="textPrimary"> Customers</Typography>
+            </Link>
+            <Link to="/invoices">
+              <Typography variant="h6" color="textPrimary"> Invoices</Typography>
+            </Link>
+            <Link to="/newinvoice">
+              <Typography variant="h6" color="textPrimary"> + New Invoice</Typography>
+            </Link>
+          </Toolbar>
+        </AppBar>
+
+      </div>
+    );
   }
-
-
-  return (
-    <div >
-      <AppBar position="static">
-        <Toolbar variant="dense" style={toolbarStyles}>
-          <Link to="/">
-            <Typography variant="h6" color="textPrimary"> Logo</Typography>
-          </Link>
-          <Link to="/products">
-            <Typography variant="h6" color="textPrimary"> Products</Typography>
-          </Link>
-          <Link to="/customers">
-            <Typography variant="h6" color="textPrimary"> Customers</Typography>
-          </Link>
-          <Link to="/invoices">
-            <Typography variant="h6" color="textPrimary"> Invoices</Typography>
-          </Link>
-          <Link to="/newinvoice">
-            <Typography variant="h6" color="textPrimary"> + New Invoice</Typography>
-          </Link>
-        </Toolbar>
-      </AppBar>
-
-    </div>
-  );
 }
+
+export default NavTabs;
