dd21f2bbc55e86cc90ca74000012023f85cbe310
ZI <ZDubinaQ@gmail.com>
2019-08-13 16:05:44 +0300

    upd(Invoices:Invoices) removed unnecessary styling

--- src/components/Invoices/Invoices.js
+++ src/components/Invoices/Invoices.js
@@ -1,61 +1,7 @@
 import React from 'react';
-
 import Content from './CommonContent';
-// import ColorButtonYellow from "../UI/Buttons/ColorButtonYellow";
-// import { makeStyles } from '@material-ui/core/styles';
-
-
-// import { Link } from 'react-router-dom';
-
-
-// function rand() {
-//   return Math.round(Math.random() * 20) - 10;
-// }
-
-// function getModalStyle() {
-//   const top = 50 + rand();
-//   const left = 50 + rand();
-//
-//   return {
-//     top: `${top}%`,
-//     left: `${left}%`,
-//     transform: `translate(-${top}%, -${left}%)`,
-//   };
-// }
-
-// const useStyles = makeStyles(theme => ({
-//   paper: {
-//     position: 'absolute',
-//     width: 400,
-//     backgroundColor: theme.palette.background.paper,
-//     borderRadius: '5px',
-//     boxShadow: theme.shadows[5],
-//     padding: theme.spacing(2, 4, 4),
-//   },
-//   buttons: {
-//     marginTop: theme.spacing(2),
-//     display: 'flex',
-//     justifyContent: 'center'
-//   }
-// }));
-
 
 export default function Invoices() {
-
-  // const classes = useStyles();
-  //
-  // const [modalStyle] = React.useState(getModalStyle);
-  // const [open, setOpen] = React.useState(false);
-  //
-  // const handleOpen = () => {
-  //   setOpen(true);
-  // };
-  //
-  // const handleClose = () => {
-  //   setOpen(false);
-  // };
-
-
 
   return (
     <Content   />
