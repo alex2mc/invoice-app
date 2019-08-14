0e28c8e3fb5421b736336b183f0533fb83541a84
ZI <ZDubinaQ@gmail.com>
2019-08-14 19:01:14 +0300

    feat(components:NavTabs:NavTabs) improved UI by adding container

--- src/components/NavTabs/NavTabs.js
+++ src/components/NavTabs/NavTabs.js
@@ -3,6 +3,7 @@ import React, {Component} from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
 
 import { Link, withRouter } from 'react-router-dom';
 import {connect} from 'react-redux';
@@ -26,28 +27,30 @@ class NavTabs extends Component {
     return (
       <div>
         <AppBar position="static" >
-          <Toolbar variant="dense" style={toolbarStyles}>
-            <Link to="/">
-              <Typography variant="h6" color="textPrimary"> Logo</Typography>
-            </Link>
-            <Link to="/products">
-              <Typography variant="h6" color="textPrimary"> Products</Typography>
-            </Link>
-            <Link to="/customers">
-              <Typography variant="h6" color="textPrimary"> Customers</Typography>
-            </Link>
-            <Link to="/invoices">
-              <Typography variant="h6" color="textPrimary"> Invoices ({invoicesAmount})</Typography>
-            </Link>
-            {
-              this.props.location.pathname !== "/newinvoice"
-                ?
-                <Link to="/newinvoice">
-                  <Typography variant="h6" color="textPrimary"> + New Invoice</Typography>
-                </Link>
-                : null
-            }
-          </Toolbar>
+          <Container>
+            <Toolbar variant="dense" style={toolbarStyles}>
+              <Link to="/">
+                <Typography variant="h6" color="textPrimary"> Logo</Typography>
+              </Link>
+              <Link to="/products">
+                <Typography variant="h6" color="textPrimary"> Products</Typography>
+              </Link>
+              <Link to="/customers">
+                <Typography variant="h6" color="textPrimary"> Customers</Typography>
+              </Link>
+              <Link to="/invoices">
+                <Typography variant="h6" color="textPrimary"> Invoices ({invoicesAmount})</Typography>
+              </Link>
+              {
+                this.props.location.pathname !== "/newinvoice"
+                  ?
+                  <Link to="/newinvoice">
+                    <Typography variant="h6" color="textPrimary"> + New Invoice</Typography>
+                  </Link>
+                  : null
+              }
+            </Toolbar>
+          </Container>
         </AppBar>
 
       </div>
