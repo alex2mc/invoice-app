82ba16d3b62ec356b146af6996b0dc5d516e2303
SashaPltnchnk <ZDubinaQ@gmail.com>
2019-08-28 08:14:28 +0300

    upd(store:customers:reducers) changed customers state

--- src/store/customers/reducers.js
+++ src/store/customers/reducers.js
@@ -4,7 +4,11 @@ import {
 
 const initialState = {
   customers: [],
-  customer: null,
+  // customers: {
+  //   entities: {[id]: customer},
+  //   ids: [id, id],
+  //   currentId: id
+  // }
 };
 
 export default function customersReducer(state = initialState, action) {
@@ -12,6 +16,24 @@ export default function customersReducer(state = initialState, action) {
   switch (action.type) {
 
     case GET_CUSTOMERS_SUCCEEDED: {
+      const customers = action.payload
+
+      const aaaa = customers.reduce((acc, customer) => {
+          return {
+            ...acc,
+            ids: [...acc.ids, customer._id],
+            entities: {
+              ...acc.entities,
+              [customer._id]: customer
+            }
+          }
+          // acc.ids.push(customer._id);
+          // acc.entities[customer._id] = customer
+        // ({...acc, ...{[`${customer._id}`]: customer}})
+
+            }, {entities: {}, ids: []})
+
+      console.log(aaaa)
       return {
         ...state,
           customers: action.payload
