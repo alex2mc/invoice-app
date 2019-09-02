7045b4c48902edcc740a80515a38500b8304bd29
alex <Zinaida.d@2muchcoffee.com>
2019-09-02 13:01:21 +0300

    upd(components:Invoices:CreateInvoice) folder  was created. CreateInvoice&Form&Field was moved to CtreateInvoice folder from(components:Invoices:Modes)

--- src/Router.js
+++ src/Router.js
@@ -5,7 +5,7 @@ import MainPage from './components/MainPage/MainPage'
 import Products from './components/Products/Products';
 import Customers from './components/Customers/Customers';
 import Invoices from './components/Invoices/Invoices';
-import CreateMode from './components/Invoices/Modes/CreateMode';
+import CreateInvoice from './components/Invoices/CreateInvoice/CreateInvoice';
 import ViewMode from './components/Invoices/Modes/ViewMode'
 import EditMode from "./components/Invoices/Modes/EditMode";
 
@@ -21,7 +21,7 @@ export default function router() {
       <Route exact path='/products' component={Products} />
       <Route exact path='/customers' component={Customers} />
       <Route exact path='/invoices' component={Invoices} />
-      <Route exact path='/newinvoice' component={CreateMode} />
+      <Route exact path='/newinvoice' component={CreateInvoice} />
     </>
   );
 }
