6ff9ce47d5b4280ff8e5d66897f00c8cc34b4fc6
alex <Zinaida.d@2muchcoffee.com>
2019-09-04 12:20:30 +0300

    refactored (components:Invoices:Invoices) into functional componenent

--- src/components/Invoices/Invoices.js
+++ src/components/Invoices/Invoices.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useEffect } from 'react';
 
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
@@ -6,12 +6,13 @@ import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
 
-import { connect } from 'react-redux';
+import { connect, useSelector } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { getInvoices } from '../../store/invoices/actions';
+import { getCustomers } from '../../store/customers/actions';
 
 import { getInvoicesArray } from '../../store/invoices/selectors';
-import { getEntities as getCustomers } from '../../store/customers/selectors';
+import { getEntities as getCustomersEntities } from '../../store/customers/selectors';
 
 import Invoice from "./Invoice";
 import StyledTableCell from "../UI/Table/StyledTableCell";
@@ -23,64 +24,51 @@ import { styles } from './styles';
 
 
 
-class CommonContent extends Component {
+const CommonContent = ({getInvoices, getCustomers, ...props}) => {
+  useEffect(() => {
+    getInvoices();
+  }, []);
 
-  componentDidMount() {
-    // this.props.fetchInvoices();
-  }
+  useEffect(() => {
+    getCustomers();
+  }, []);
 
+  const customers = useSelector(state => getCustomersEntities(state))
+  const invoices = useSelector(state => getInvoicesArray(state))
 
-  render() {
-
-    // console.log(this.props.invoices)
-
-    const {
-      invoices,
-      customers
-    } = this.props;
-
-
-
-
-    const invoicesRows  =  invoices
-      ? invoices.map(invoice => (
-        <Invoice key={invoice._id} customers={customers} inv_id={invoice._id} {...invoice} /> ))
-      : null;
+  const invoicesRows  =  invoices
+    ? invoices.map(invoice => (
+      <Invoice key={invoice._id} customers={customers} inv_id={invoice._id} {...invoice} /> ))
+    : null;
 
 
-    return (
-      <Paper style={styles.root}>
-        <Table style={styles.table}>
-          <TableHead>
-            <TableRow>
-              <StyledTableCell>Invoiсe ID</StyledTableCell>
-              <StyledTableCell>Customer Name</StyledTableCell>
-              <StyledTableCell>Discount (%)</StyledTableCell>
-              <StyledTableCell>Total</StyledTableCell>
-              <StyledTableCell align="center">Actions</StyledTableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
+  return (
+    <Paper style={styles.root}>
+      <Table style={styles.table}>
+        <TableHead>
+          <TableRow>
+            <StyledTableCell>Invoiсe ID</StyledTableCell>
+            <StyledTableCell>Customer Name</StyledTableCell>
+            <StyledTableCell>Discount (%)</StyledTableCell>
+            <StyledTableCell>Total</StyledTableCell>
+            <StyledTableCell align="center">Actions</StyledTableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
 
-            {invoicesRows}
+          {invoicesRows}
 
-          </TableBody>
-        </Table>
-      </Paper>
-    );
-  }
+        </TableBody>
+      </Table>
+    </Paper>
+  );
 }
 
-const mapStateToProps = state => {
-  return {
-    invoices: getInvoicesArray(state),
-    customers: getCustomers(state),
-  }
-}
 
 const mapDispatchToProps = dispatch =>
   bindActionCreators({
-    getInvoices
+    getInvoices,
+    getCustomers
   }, dispatch);
 
-export default connect(mapStateToProps, mapDispatchToProps)(CommonContent);
+export default connect(null, mapDispatchToProps)(CommonContent);
