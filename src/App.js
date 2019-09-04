acddd292a0f8036112eb0eeb5331c64d8bf440da
alex <Zinaida.d@2muchcoffee.com>
2019-09-04 12:24:21 +0300

    refactored (src:App) into functional componenent

--- src/App.js
+++ src/App.js
@@ -1,50 +1,16 @@
-import React, {Component} from 'react';
-
+import React from 'react';
 import { BrowserRouter } from 'react-router-dom'
 import Router from './Router'
-
 import NavTabs from './components/NavTabs/NavTabs'
-
-import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
-
-
 import './App.css';
-import { getProducts } from './store/products/actions';
-import { getCustomers } from './store/customers/actions';
-import { getInvoices } from './store/invoices/actions';
 
 
 
-class App extends Component {
-
- componentDidMount() {
-   this.props.getCustomers();
-   this.props.getProducts();
-   this.props.getInvoices()
- }
-
-  render () {
-
+export default function App() {
   return (
     <BrowserRouter>
         <NavTabs />
         <Router />
     </BrowserRouter>
   );
-}}
-
-// const mapStateToProps = state => ({ ...state });
-//
-const mapDispatchToProps = dispatch =>
-  bindActionCreators({
-    getCustomers,
-    getProducts,
-    getInvoices
-  }, dispatch);
-
-
-export default
-connect(null, mapDispatchToProps)(
-  App
-);
+}
